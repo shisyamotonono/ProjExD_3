c6a9fe8381dc26cf688c9218b2c0aa0c3eef9495
shisyamotonono <c0b221579f@edu.teu.ac.jp>
2024-06-25 14:43:08 +0900

    練習３：よろこびエフェクト

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -161,15 +161,18 @@ def main():
             pg.display.update()
             time.sleep(1)
             return
+        
         if bomb is not None:
             if beam is not None:
                 if bomb.rct.collidedict(beam.rct):
                     bomb = None
                     beam = None
+                    bird.change_img(6, screen)
+                    
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
         if beam is not None:
-            beam.update(screen) 
+            beam.update(screen)  
         if bomb is None:
             bomb.update(screen)  
         bomb.update(screen)
