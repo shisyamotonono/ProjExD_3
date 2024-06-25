90f54bf780efdbf2c3c6851234d07cdcdd1b8d5c
shisyamotonono <c0b221579f@edu.teu.ac.jp>
2024-06-25 14:52:38 +0900

    練習4：ゲームオーバー実装

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -160,6 +160,11 @@ def main():
             bird.change_img(8, screen)
             pg.display.update()
             time.sleep(1)
+            fonto = pg.font.Font(None, 80)
+            txt = fonto.render("Game Over", True, (255, 0, 0))
+            screen.blit(txt, [WIDTH/2-150, HEIGHT/2])
+            pg.display.update()
+            time.sleep(5)
             return
         
         if bomb is not None:
